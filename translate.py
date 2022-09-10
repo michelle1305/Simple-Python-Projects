717492f919ccbe0ea5222c9ccc5173f748ee25ac
michelle1305 <108060123+michelle1305@users.noreply.github.com>
2022-09-11 00:07:14 +0530

    Updated translate.py
    input options added

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 import googletrans as gt
 import termcolor as tc 
+import sys
 
 def translate(text,lang='en'):
     try:
@@ -12,8 +13,18 @@ def translate(text,lang='en'):
     
 def take_input():
     try:
-        inp = input(tc.colored('[*] Enter the Text to Translate : ','yellow'))
-        dest_lang = input(tc.colored('[*] Enter a Language in which you want to Convert :','yellow')).lower()
+        if  len(sys.argv) < 2 :
+            inp = input(tc.colored('[*] Enter the Text to Translate : ','yellow'))
+        else:
+            if (len(sys.argv[1].split('.')) <= 3) and ( 'txt' in sys.argv[1].split('.')):
+                with open(sys.argv[1], encoding = 'utf-8') as f:
+                    inp = f.read()
+                    inp = inp.replace("\n","  ")
+                    print(inp)
+            else:
+                inp = str(sys.argv[1])
+            
+        dest_lang = input(tc.colored('[*] Enter a Language in which you want to Convert : ','yellow')).lower()
         dest_code = None
         for i,j in gt.LANGUAGES.items():
             if j == dest_lang:
@@ -29,8 +40,7 @@ def take_input():
 
 if __name__ == '__main__':
     take_input()
-    
-    
+   
     
 '''
 Requirements :- 
